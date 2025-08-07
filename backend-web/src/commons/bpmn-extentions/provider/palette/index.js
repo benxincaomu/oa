f4431aaee0f7509a5a7ee1c9d3537ddf61216a73
sunft <benxincaomu@foxmail.com.cn>
2025-08-07 19:13:28 +0800

    前后端拆分

--- backend-web/src/commons/bpmn-extentions/provider/palette/index.js
+++
@@ -1,5 +0,0 @@
-import OaPaletteProvider from './OaPaletteProvider';
-export default {
-  __init__: [ 'oaPropertiesProvider' ],
-  oaPaletteProvider: [ 'type', OaPaletteProvider ],
-};
--- backend-web/src/commons/bpmn-extentions/provider/properties-panel/ModelerProps.ts
+++
@@ -1,7 +0,0 @@
-import BpmnModeler from 'camunda-bpmn-js/lib/base/Modeler';
-import React from 'react';
-
-export default interface ModelerProps {
-    bpmnModelerRef: React.RefObject<BpmnModeler | null>;
-    bpmnId?: string;
-}
